3a2639823abb82a5f4a29b69b493fd44c4fc8d28
Arun M Solanki <akashsolanki292@gmail.com>
2023-08-26 23:26:52 +0530

    26-08-2023

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const MONGO_CONNECTION_STRING = 'mongodb://localhost:27017/twitterX';
+export const PORT = 3001;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 import express from 'express';
+import { PORT, MONGO_CONNECTION_STRING } from './config';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 const app = express();
-const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// mongo db connect
+// mongoose.connect(MONGO_CONNECTION_STRING, { autoIndex: true }).then(() => {
+//   console.log('DB CONNECTED');
+// });
 
 app.get('/', (req, res) => {
-  res.send('Hello Express 5!');
+  res.send('Hello Express!');
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+app.get('/home', (req, res) => {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies);
+
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies);
+  res.json({ valid: true });
+});
+
+app.listen(PORT, () => {
+  return console.log(`Express is listening at http://localhost:${PORT}`);
 });
